bbe72651f4c9e347160908515dbc053830057695
hungtm99 <hungtm.tech99@gmail.com>
2022-08-05 18:28:30 +0700

    dich vu

--- SalonHoangCuc/SalonHoangCuc/Controllers/DichVuController.cs
+++ SalonHoangCuc/SalonHoangCuc/Controllers/DichVuController.cs
@@ -1,17 +1,41 @@
-﻿using System;
+﻿using CongViecGiaDinh.Entities;
+using CongViecGiaDinh.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
 
 namespace CongViecGiaDinh.Controllers
 {
     public class DichVuController : Controller
     {
+        Entities.DichVuEntities _db = new DichVuEntities();
         // GET: DichVu
-        public ActionResult Index()
+        public ActionResult Index(int? page)
         {
-            return View();
+            if (page == null) page = 1;
+            var links = (from l in _db.DichVu
+                         select l).OrderBy(x => x.ID);
+            List<DichVuMaping> DichVuMappinglst = new List<DichVuMaping>();
+
+            foreach (var item in links)
+            {
+                DichVuMaping dichVuMapping = new DichVuMaping();
+                dichVuMapping.ID = item.ID;
+                dichVuMapping.TenDichVu = item.TenDichVu;
+                dichVuMapping.TenTiengAnh = item.TenTiengAnh;
+                dichVuMapping.Gia = item.Gia;
+                dichVuMapping.ID_NhomDichVu = item.ID_NhomDichVu;
+
+                DichVuMappinglst.Add(dichVuMapping);
+            }
+
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+
+            return View(DichVuMappinglst.ToPagedList(pageNumber, pageSize));
         }
     }
 }
